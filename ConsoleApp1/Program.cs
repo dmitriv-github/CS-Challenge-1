56c31c991c7d837fb7b145d0e2538f174584506e
Dmitri Vassiliev <dmitri@tabbledabble.com>
2020-03-22 17:06:43 -0400

    Refactor Program to use the Task-based Asynchronous Pattern (TAP).

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,10 +12,18 @@ namespace ConsoleApp1
     {
         static string[] results = new string[50];
         static ConsoleKey key;
-        static Tuple<string, string> names;
+        static Tuple<string, string> name;
         static ConsolePrinter printer = new ConsolePrinter();
 
-        static void Main(string[] args)
+        static JokesFeed jokes;
+
+        static NamesFeed names;
+
+        static void Main(string[] args) {
+            AsyncMain(args).Wait();
+        }
+
+        static async Task AsyncMain(string[] args)
         {
             printer.printLine("Press ? to get instructions, or type 'exit' to exit.");
             string initialInput;
@@ -28,67 +36,74 @@ namespace ConsoleApp1
 
             printer.printLine("Press c to get categories");
             printer.printLine("Press r to get random jokes");
+            printer.printLine("Press x at any time to exit");
 
-            GetEnteredKey();
-
-            if (key == ConsoleKey.C)
-            {
-                getCategories();
-                PrintResults();
-            }
-            
-            if (key == ConsoleKey.R)
-            {
-                printer.printLine("Want to use a random name? y/n");
+            do {
                 GetEnteredKey();
 
-                if (key == ConsoleKey.Y) GetNames();
+                if (key == ConsoleKey.C)
+                {   
+                    printer.printLine();
 
-                printer.printLine("Want to specify a category? y/n");
+                    await getCategories();
+                    PrintResults();
+                }
                 
-                if (key == ConsoleKey.Y)
-                {
-                    printer.printLine("How many jokes do you want? (1-9)");
-                    int n = Int32.Parse(Console.ReadLine());
-                    printer.printLine("Enter a category;");
-                    GetRandomJokes(Console.ReadLine(), n);
-                    PrintResults();
-                }
-                else
-                {
-                    printer.printLine("How many jokes do you want? (1-9)");
-                    int n = Int32.Parse(Console.ReadLine());
-                    GetRandomJokes(null, n);
-                    PrintResults();
-                }
-            }
-            names = null;
+                // if (key == ConsoleKey.R)
+                // {
+                //     printer.printLine("Want to use a random name? y/n");
+                //     GetEnteredKey();
+
+                //     if (key == ConsoleKey.Y) GetNames();
+
+                //     printer.printLine("Want to specify a category? y/n");
+                    
+                //     if (key == ConsoleKey.Y)
+                //     {
+                //         printer.printLine("How many jokes do you want? (1-9)");
+                //         int n = Int32.Parse(Console.ReadLine());
+                //         printer.printLine("Enter a category;");
+                //         GetRandomJokes(Console.ReadLine(), n);
+                //         PrintResults();
+                //     }
+                //     else
+                //     {
+                //         printer.printLine("How many jokes do you want? (1-9)");
+                //         int n = Int32.Parse(Console.ReadLine());
+                //         GetRandomJokes(null, n);
+                //         PrintResults();
+                //     }
+                // }
+
+                names = null;
+
+            } while (key != ConsoleKey.X);
         }
 
         private static void PrintResults()
         {
-            printer.printLine("[" + string.Join(",", results) + "]");
+            printer.printLine("[" + string.Join(", ", results) + "]");
         }
 
         private static ConsoleKey GetEnteredKey() => key = Console.ReadKey().Key;
 
-        private static void GetRandomJokes(string category, int number)
+        // private static void GetRandomJokes(string category, int number)
+        // {
+        //     new JsonFeed("https://api.chucknorris.io", number);
+        //     results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category);
+        // }
+
+        private static async Task getCategories()
         {
-            new JsonFeed("https://api.chucknorris.io", number);
-            results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category);
+            jokes = jokes ?? new JokesFeed();
+            results = await jokes.getCategories();
         }
 
-        private static void getCategories()
-        {
-            new JsonFeed("https://api.chucknorris.io", 0);
-            results = JsonFeed.GetCategories();
-        }
-
-        private static void GetNames()
-        {
-            new JsonFeed("http://uinames.com/api/", 0);
-            dynamic result = JsonFeed.Getnames();
-            names = Tuple.Create(result.name.ToString(), result.surname.ToString());
-        }
+        // private static void GetNames()
+        // {
+        //     new JsonFeed("http://uinames.com/api/", 0);
+        //     dynamic result = JsonFeed.Getnames();
+        //     names = Tuple.Create(result.name.ToString(), result.surname.ToString());
+        // }
     }
 }
